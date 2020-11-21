a5224081b71d9bbd15ca9fbaf674a03da45b562c
Pascal Le Merrer <pascal.lemerrer@orange.com>
2020-11-21 13:43:35 +0100

    Add Image Search solution for step 2

--- solutions/elm-search/src/Main.elm
+++ solutions/elm-search/src/Main.elm
@@ -1,15 +1,48 @@
 module Main exposing (main)
 
-import Html exposing (div, h1, input, text)
+import Browser
+import Html exposing (Html, div, h1, input, text)
 import Html.Attributes exposing (class, type_)
+import Html.Events exposing (onInput)
 
 
-view =
+type Msg
+    = UserChangedInput String
+
+
+type alias Model =
+    String
+
+
+initialModel : Model
+initialModel =
+    ""
+
+
+view : Model -> Html Msg
+view model =
     div [ class "container" ]
         [ h1 [ class "title" ] [ text "elm image search" ]
-        , input [ type_ "text", class "medium input" ] []
+        , input
+            [ type_ "text"
+            , class "medium input"
+            , onInput UserChangedInput
+            ]
+            []
         ]
 
 
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        UserChangedInput value ->
+            value
+
+
+main : Program () Model Msg
 main =
-    view
+    Browser.sandbox
+        { init = initialModel
+        , view = view
+        , update = update
+        }
